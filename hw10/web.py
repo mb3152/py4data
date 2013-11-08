93f253699865776cebab7454158be84093e6a49c
Maxwell Bertolero <bertolero@berkeley.edu>
2013-11-08 13:53:44 -0800

    got the upload to work.

---
+++ hw10/web.py
@@ -0,0 +1,20 @@
+import sqlite3
+from flask import g
+
+DATABASE = '/path/to/database.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = connect_to_database()
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+@app.route('/')
+def index():
+    cur = get_db().cursor()
