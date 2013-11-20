2ed735c57eac5ac42087062d7740068f7ba9cf6b
Maxwell Bertolero <bertolero@berkeley.edu>
2013-11-20 10:49:52 -0800

    changed setup.py to work with other module

--- hw2/setup.py
+++ hw2/setup.py
@@ -2,6 +2,28 @@
 
 from distutils.core import setup 
 
-setup(name='CalCalc',
+setup(name='grow',
       version='1.0',
-      py_modules=['CalCalc'],)
+      py_modules=['grow'],)
+
+import os
+
+#oct2py
+os.system('pip install oct2py')
+#homebrew
+os.system('ruby -e "$(curl -kfsSL https://raw.github.com/mxcl/homebrew/go')
+
+#Get Octave! 
+os.system('brew tap homebrew/science')
+os.system('brew update && brew upgrade')
+os.system('brew install gfortran')
+os.system('brew install octave')
+
+#Get py2neo
+os.system('pip install py2neo')
+______________________________________________
+#Get neo4j
+os.system('brew install neo4j')
+
+print 'Installation done!'
+
